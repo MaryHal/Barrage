a0c1302fbb1c7881dc08e1f0ed80aca49304a73c
MaryHal <maryhal@users.noreply.github.com>
2015-04-24 00:53:49 -0700

    Renamed barrage scripts, more advanced love test

--- example/main.lua
+++ example/main.lua
@@ -3,10 +3,34 @@ local barrage = require "barrageC"
 local bulletImg = nil
 local b = nil
 
+local barrageIndex = 1
+local barrageFilelist = nil
+
+local font = nil
+
 function love.load(arg)
    bulletImg = love.graphics.newImage('assets/bullet.png')
-   barrage.setRngSeed()
-   b = barrage.new(arg[2], 320.0, 120.0)
+
+   font = love.graphics.newFont(12)
+   barrageFileList = love.filesystem.getDirectoryItems('barrage')
+
+   b = barrage.new('barrage/' .. barrageFileList[barrageIndex], 320.0, 120.0)
+end
+
+function love.keypressed(key)
+   if key == ' ' then
+      b = barrage.new('barrage/' .. barrageFileList[barrageIndex], 320.0, 120.0)
+   elseif key == 'left' then
+      barrageIndex = barrageIndex - 1
+      if barrageIndex < 1 then
+         barrageIndex = #barrageFileList
+      end
+   elseif key == 'right' then
+      barrageIndex = barrageIndex + 1
+      if barrageIndex > #barrageFileList then
+         barrageIndex = 1
+      end
+   end
 end
 
 function love.update(dt)
@@ -17,6 +41,8 @@ function love.update(dt)
 end
 
 function love.draw(dt)
+   love.graphics.setFont(font)
+   love.graphics.print(barrageFileList[barrageIndex], 8, 8)
    while b:hasNext() do
       local x, y = b:yield()
       love.graphics.draw(bulletImg, x, y, 0, 0.5, 0.5)
